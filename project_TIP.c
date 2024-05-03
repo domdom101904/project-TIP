73a7e30aca756c7cb7aa1b550628df0757562e2f
domdom101904 <159731847+domdom101904@users.noreply.github.com>
2024-05-03 18:38:38 +0800

    small notes for accounting

---
+++ project_TIP.c
@@ -0,0 +1,89 @@
+//accounting cycle and accounting concepts
+
+//normal accounts
+//assets (debit +, credit -)
+//liabilities (debit -, credit +)
+//equity (debit -, credit +)
+//expenses (debit +, credit -)
+//income (debit -, credit+)
+
+//accounting equation
+    //assets = liabilities + equity
+
+//account titles
+    //normal asset titles (periodic inventory)
+        //cash
+        //accounts receivable
+        //merchandise inventory
+        //supplies
+        //prepaid insurance
+        //prepaid expense
+        //land
+        //building
+        //equipment
+            //accumulated depreciation (credit normal account)
+
+    //normal liability titles
+        //accounts payable
+
+    //normal equity titles
+        //owner name, capital
+        //owner name, withdrawals
+
+    //normal income accounts
+        //sales
+        //sales returns and allowances (debit normal account)
+        //sales discounts (debit normal account)
+
+        //purchases
+        //purchases returns and allowances (credit normal account)
+        //purchase discounts (credit normal account)
+
+    //normal expense accounts
+        //transportation in
+        //salaries expense
+        //transportation out
+        //advertising expense
+        //interest expense
+        //miscellaneous expense
+        //advertising expense
+
+//preparation of financial statements
+    //income statement
+        //responsible for calculating the profit
+            //gross income minus total expenses
+                //gross income is computed through net sales minus cost of goods sold
+                    //gross sales is computed through gross sales minus sales discount and sales returns and allowances
+                    //cost of goods sold is computed through TGAS minus ending inventory
+                        //total goods available for sale (TGAS) is computed through total inventory, beginning inventory, minus purchase discounts and purchase returns and allowances
+
+    //statement of changes in equity
+        //calculated through beginning capital minus net loss
+            //new capital is calculated with previous total minus withdrawals of the current period
+
+    //statement of financial position or balance sheet
+        //calculates total assets, total liabilities and owner's equity
+            //total assets is totalled with current assets and noncurrent assets
+            //total LE is computed with total current liabilities, total noncurrent liabilities and current capital
+        
+    //statement of cash flows
+        //collection of all cash inflows and outflows of the business
+        //mainly deals with short descriptions describing why cash went in or out
+        //categorizes descriptions into 3
+            //operating activities
+                //mainly the selling and purchasing of current things
+            //investing activities
+                //mainly the purchasing of equipment
+            //financing activities
+                //mainly the financing of the business, such as sponsors, investors, etc., or the withdrawal of cash from the business
+
+//closing entries
+    //the program will automatically do the closing entries for the user
+
+//main focus of the project is to calculate the purchases and sales through journal transactions made by the user; inventory represented in financial terms
+    //additionally, the project aims to summarize everything for the sole proprietor, as most journalized transactions would rely on purchases and sales
+    //equipment, land, etc. would be described in advance, unless the user wants to have changes
+
+    //checking of variables and reliability of the program will be done through trial balances; trial balance, adjusted trial balance, and post-closing trial balance
+
+    //will add more details especially sa relevant accounting titles used in sari-sari stores to ensure efficiency of the code
